67a2dbf731ad8d78428be443a2a19eb101899504
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-13 18:40:02 -0300

    feat: initial app setup.

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import authorRoutes from "./routes/authorRoutes";
+
+const app = express();
+app.use(express.json());
+
+app.use("/authors", authorRoutes);
+
+export default app;

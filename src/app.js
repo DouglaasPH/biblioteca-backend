6dbcd8d0b68813a2c9d004a65a57364d62bb59f9
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-13 22:27:08 -0300

    feat: add book to app.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authorRoutes from "./routes/authorRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/authors", authorRoutes);
+app.use("/books", bookRoutes);
 
 export default app;

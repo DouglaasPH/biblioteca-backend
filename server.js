1550bb8002aaf0a82831546d75ebb2e0eb7a62d2
Douglas Phelipe <douglasphelipedpss@gmail.com>
2025-12-15 01:23:32 -0300

    feat and fix: add policy CORS and port 3001.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./src/app.js";
 
-app.listen(3000, () => {
-  console.log("API rodando na porta 3000");
+app.listen(3001, () => {
+  console.log("API rodando na porta 3001");
 });
--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
+
 import authorRoutes from "./routes/authorRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 
 const app = express();
+
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use("/authors", authorRoutes);
 app.use("/books", bookRoutes);
